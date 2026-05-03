12c8114484e751a08913edf8ff14c66678502673
Gerson Juárez <111896881+JuarezGer@users.noreply.github.com>
2026-05-03 12:29:11 -0600

    Menú de pacientes y doctores (acitivity's)

--- app/src/main/java/ues/fia/proyecto1pdm115/MainActivity.java
+++ app/src/main/java/ues/fia/proyecto1pdm115/MainActivity.java
@@ -1,20 +1,41 @@
 package ues.fia.proyecto1pdm115;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+
+import ues.fia.proyecto1pdm115.pacientes.*;
+import ues.fia.proyecto1pdm115.doctores.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout cardPacientes;
+    LinearLayout cardDoctores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Navegación a Pacientes
+        cardPacientes = findViewById(R.id.cardPacientes);
+        cardPacientes.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PacienteMenuActivity.class);
+            startActivity(intent);
+        });
+        //Navegación a Doctores
+        cardDoctores = findViewById(R.id.cardDoctores);
+        cardDoctores.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, DoctorMenuActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainLayout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
