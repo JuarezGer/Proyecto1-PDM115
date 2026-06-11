59f58b085a65086d115727f010aaa3b04507ad56
Gerson Juárez <111896881+JuarezGer@users.noreply.github.com>
2026-06-11 16:13:10 -0600

    Cambios Finales

--- app/src/main/java/ues/fia/proyecto1pdm115/ApiConfig.java
+++ app/src/main/java/ues/fia/proyecto1pdm115/ApiConfig.java
@@ -6,7 +6,8 @@ public class ApiConfig {
 
     //public static final String BASE_URL = "http://10.32.28.71/hospital_api/";
     //public static final String BASE_URL = "http://192.168.0.8/hospital_api/";
-    public static final String BASE_URL = "http://192.168.1.97/hospital_api/";
+    //public static final String BASE_URL = "http://192.168.1.97/hospital_api/";
+    public static final String BASE_URL = "http://192.168.18.12/hospital_api/";
 
     public static final String REGISTER_USER = BASE_URL + "register_user.php";
     public static final String GET_USER = BASE_URL + "get_user.php";
